5e65f3aa281002bc5684dde09ae81d6528355814
Onur Erinç ŞENOL <82088135+ErincSENOL@users.noreply.github.com>
2021-11-14 12:59:39 +0300

    Add files via upload

---
+++ MVC_Core_JQuery_Controller_Get/MVC_Core_JQuery_Controller_Get/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using MVC_Core_JQuery_Controller_Get.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Core_JQuery_Controller_Get.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.genres = new SelectList(new[]
+            {
+                new { id = "" , name = "All"},
+                new { id = "1" , name = "Erkek"},
+                new { id = "2" , name = "Kadın"},
+                new { id = "3" , name = "Belirtilmemiş"},
+            },
+            "id", "name", "selected_genres");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_Core_JQuery_Controller_Get/MVC_Core_JQuery_Controller_Get/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Core_JQuery_Controller_Get.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
